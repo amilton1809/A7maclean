08731a7a04cbdbc31f66aba27bd8703d418ade38
amilton1809 <a.milton18@hotmail.com>
2025-04-14 20:01:19 -0300

    att com novas interações

--- funcao.js
+++ funcao.js
@@ -12,4 +12,20 @@
     });
   });
 
+  const btnTop = document.getElementById("btn-top");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      btnTop.classList.add("show");
+    } else {
+      btnTop.classList.remove("show");
+    }
+  });
+
+  btnTop.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
 
